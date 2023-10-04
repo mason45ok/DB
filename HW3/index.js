de1f52c8ff816a04233f086502cf98ca452f0c44
mason45ok <128038735+mason45ok@users.noreply.github.com>
2023-10-04 14:01:39 +0800

    HW3

--- HW3/index.js
+++ HW3/index.js
@@ -1,16 +1,23 @@
-var Express = require("express");
-var Mongoclient=require("mongodb").MongoClient;
-var cors=require("cors");
-const multer=require("multer")
-
-var app=Express();
-app.use(cors());
-var CONNECTON_STRING="mongodb+srv://mason45ok:%40Mason45ok@testdb.wgzvgzj.mongodb.net/?retryWrites=true&w=majority";
-var DATABASENAME="testDB";
-var database;
-app.listen(5038,()=>{
-    Mongoclient.connect(CONNECTON_STRING,(error,client)=>{
-        database=client.db(DATABASENAME);
-        console.log("Mongo DB Connection Successful");
-    })
-})
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mason45ok:%40Mason45ok@testdb.wgzvgzj.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
--- HW3/server/index.js
+++ HW3/server/index.js
@@ -1,165 +1,17 @@
 const express = require("express");
 const app = express();
-const mysql = require("mysql");
 const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
 
-const db = mysql.createConnection({
-  user: "root",
-  host: "localhost",
-  password: "@Mason45ok",
-  database: "student-course", // 这里使用你的数据库名称
-});
-
-// 创建课程
-app.post("/createCourse", (req, res) => {
-  const course_id = req.body.course_id;
-  const course_name = req.body.course_name;
-
-  db.query(
-    "INSERT INTO courses (course_id, course_name) VALUES (?, ?)",
-    [course_id, course_name],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Course Inserted");
-      }
-    }
-  );
-});
-
-// 获取课程列表
-app.get("/courses", (req, res) => {
-  db.query("SELECT * FROM courses", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-// 创建学生
-app.post("/createStudent", (req, res) => {
-  const student_id = req.body.student_id;
-  const student_name = req.body.student_name;
-
-  db.query(
-    "INSERT INTO students (student_id, student_name) VALUES (?, ?)",
-    [student_id, student_name],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Student Inserted");
-      }
-    }
-  );
-});
-
-// 获取学生列表
-app.get("/students", (req, res) => {
-  db.query("SELECT * FROM students", (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-app.listen(3001, () => {
-  console.log("Yey, your server is running on port 3001");
-});
-// 更新课程
-app.put("/updateCourse", (req, res) => {
-  const course_id = req.body.course_id;
-  const course_name = req.body.course_name;
-
-  db.query(
-    "UPDATE courses SET course_name = ? WHERE course_id = ?",
-    [course_name, course_id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Course Updated");
-      }
-    }
-  );
-});
-
-// 更新学生
-app.put("/updateStudent", (req, res) => {
-  const student_id = req.body.student_id;
-  const student_name = req.body.student_name;
-
-  db.query(
-    "UPDATE students SET student_name = ? WHERE student_id = ?",
-    [student_name, student_id],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.send("Student Updated");
-      }
-    }
-  );
-});
-// 删除课程
-app.delete("/deleteCourse/:id", (req, res) => {
-  const courseId = req.params.id;
-
-  db.query("DELETE FROM courses WHERE course_id = ?", courseId, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send("Course Deleted");
-    }
-  });
-});
-
-// 删除学生
-app.delete("/deleteStudent/:id", (req, res) => {
-  const studentId = req.params.id;
-
-  db.query("DELETE FROM students WHERE student_id = ?", studentId, (err, result) => {
-    if (err) {
-      console.log(err);
-    } else {
-      res.send("Student Deleted");
-    }
-  });
-});
-app.get("/searchstudent", (req, res) => {
-  const searchQuery = req.query.search || "";
-
-  const sqlQuery = `
-  SELECT 
-    students.student_name,
-    student_courses.course_id,
-    courses.course_name
-    FROM student_courses
-    INNER JOIN students ON students.student_id = student_courses.student_id
-    INNER JOIN courses ON student_courses.course_id = courses.course_id
-    WHERE students.student_name LIKE ?
-    OR student_courses.course_id LIKE ?
-    OR courses.course_name LIKE ?
-   `;
-
-  db.query(
-    sqlQuery,
-    [`%${searchQuery}%`, `%${searchQuery}%`, `%${searchQuery}%`],
-    (err, result) => {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Error fetching search results");
-      } else {
-        res.send(result);
-      }
-    }
-  );
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://mason45ok:%40Mason45ok@testdb.wgzvgzj.mongodb.net/?retryWrites=true&w=majority";
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
