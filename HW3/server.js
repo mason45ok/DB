14b526a6def10bef0f7c5abee9260f9cdd69afb4
mason45ok <128038735+mason45ok@users.noreply.github.com>
2023-10-11 14:32:41 +0800

    Update server.js

--- HW3/server.js
+++ HW3/server.js
@@ -1,18 +1,22 @@
 var express =require('express')
 var server = express();
-var mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect("mongodb+srv://mason45ok:%40Mason45ok@testdb.wgzvgzj.mongodb.net/?retryWrites=true&w=majority/est"{useNewUrlParser: true, useUnifiedTopology: true},function checkDB()
-{
-    if(error)
-    {
-        console.log("error")
-    }
-    else
-    {
-        console.log("DB connected")
-    }
+mongoose.connect("mongodb+srv://mason45ok:%40Mason45ok@testdb.wgzvgzj.mongodb.net/?retryWrites=true&w=majority/est", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
+
+const db = mongoose.connection;
+
+db.on('error', (error) => {
+  console.error('Error connecting to MongoDB:', error);
+});
+
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
 
 
 
